4c15815d59b7e3c630c81262d4dcdab78a141634
Jonathon Gardner <theAppGardner@gmail.com>
2021-12-03 17:38:23 +0000

    Fix docs

--- autostart_systemd.sh
+++ autostart_systemd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp garageqtpi@pi.service /etc/systemd/system/sensorqtpi@${SUDO_USER:-${USER}}.service
+cp sensorqtpi@pi.service /etc/systemd/system/sensorqtpi@${SUDO_USER:-${USER}}.service
 sed -i "s?/home/pi/SensorQTPi?`pwd`?" /etc/systemd/system/sensorqtpi@${SUDO_USER:-${USER}}.service
 systemctl --system daemon-reload
 systemctl enable sensorqtpi@${SUDO_USER:-${USER}}.service
